3db1884beaea358d812790da491caf1c671490a8
rus-collab <106526213+rus-collab@users.noreply.github.com>
2022-05-30 16:19:02 +0530

    Update Program.cs
    Changes by rustamshai@yahoo.com

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 //Fork
+//Changes by rustamshai@yahoo.com
 namespace myWebApp
 {
     public class Program
