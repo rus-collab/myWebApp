f0b772f449194c6b151b319d865d5aeadd79cd0b
rustamshai <83416953+rustamshai@users.noreply.github.com>
2022-05-30 16:16:39 +0530

    Update Program.cs
    Fork changes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+//Fork
 namespace myWebApp
 {
     public class Program
